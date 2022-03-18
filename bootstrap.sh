67a172faeb7bca73fb3e9adaee72b9bbb754f29b
Ian Eure <ian@even.com>
2022-03-18 09:59:20 -0700

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,7 +35,6 @@ mv even-interview-bootstrap-main/* .
 rmdir even-interview-bootstrap-main
 git init -b main .
 git remote add origin git@github.com:$REPO
-# Let's not make things harder than they need to be.
 
 ./ssh-key.sh
 ./devsetup.sh
