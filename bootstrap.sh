f791570eaa2d514de97d987561bd345c49a0cf37
Ian Eure <ian@even.com>
2022-03-18 09:42:10 -0700

    WIPs

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,16 @@
 
 ZIPF=https://github.com/ieure/even-interview-bootstrap/archive/refs/heads/main.zip
 
+if [ -e "$REPO" ]; then
+    echo "Sorry, the repo name wasn't specified, so I can't do anything."
+    exit 1
+fi
+
+NAME=$(echo $REPO | cut -d/ -f2)
+
+mkdir $NAME
+cd $NAME
+
 if [ -x wget ]; then
     wget $ZIPF;
 elif [ -x curl ]; then
@@ -13,11 +23,13 @@ fi
 
 unzip main.zip
 rm main.zip
-cd even-interview-bootstrap-main
+mv even-interview-bootstrap-main/* .
+rmdir even-interview-bootstrap-main
 git init .
 git remote add origin git@github.com:$REPO
 
 ./ssh-key.sh
+./devsetup.sh
 
 git add .
 git commit -am "Added bootstrap repo"
